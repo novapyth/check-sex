bf844f670356f02aaab55cea92f97870e570d1fe
novapyth <draabhishekkumar@gmail.com>
2022-06-16 11:18:48 +0200

    Update check_sex_samtools.sh

--- check_sex_samtools.sh
+++ check_sex_samtools.sh
@@ -3,9 +3,9 @@
 #xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3)/$(samtools idxstats $1 | grep "X" | cut -f 2)" | bc)
 #original with Y
 #ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3)/$(samtools idxstats $1 | grep "Y" | cut -f 2)" | bc)
-# changed for with "chrX" 
+# changed for "chrX" 
 xcov=$(echo "scale=4; $(samtools idxstats $1 | grep -w "chrX" | cut -f 3)/$(samtools idxstats $1 | grep -w "chrX" | cut -f 2)" | bc)
-
+# changed for "chrY" 
 ycov=$(echo "scale=4; $(samtools idxstats $1 | grep -w "chrY" | cut -f 3)/$(samtools idxstats $1 | grep -w "chrY" | cut -f 2)" | bc)
 
 rat=$(echo "scale=4; ${xcov}/${ycov}" | bc)
