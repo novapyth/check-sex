3fb08fb501da27c203f720c497b96fc739a20688
novapyth <draabhishekkumar@gmail.com>
2022-06-16 11:17:34 +0200

    Updated check_sex_samtools.sh
    regarding chromosome in chrX and chrY format

--- check_sex_samtools.sh
+++ check_sex_samtools.sh
@@ -1,4 +1,13 @@
-xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3)/$(samtools idxstats $1 | grep "X" | cut -f 2)" | bc)
-ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3)/$(samtools idxstats $1 | grep "Y" | cut -f 2)" | bc)
+
+#original with X
+#xcov=$(echo "scale=4; $(samtools idxstats $1 | grep "X" | cut -f 3)/$(samtools idxstats $1 | grep "X" | cut -f 2)" | bc)
+#original with Y
+#ycov=$(echo "scale=4; $(samtools idxstats $1 | grep "Y" | cut -f 3)/$(samtools idxstats $1 | grep "Y" | cut -f 2)" | bc)
+# changed for with "chrX" 
+xcov=$(echo "scale=4; $(samtools idxstats $1 | grep -w "chrX" | cut -f 3)/$(samtools idxstats $1 | grep -w "chrX" | cut -f 2)" | bc)
+
+ycov=$(echo "scale=4; $(samtools idxstats $1 | grep -w "chrY" | cut -f 3)/$(samtools idxstats $1 | grep -w "chrY" | cut -f 2)" | bc)
+
 rat=$(echo "scale=4; ${xcov}/${ycov}" | bc)
+
 echo "X:Y ratio: $rat"
